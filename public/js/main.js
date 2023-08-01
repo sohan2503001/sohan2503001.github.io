6312db4e4b27a927a46a8e95de96ce2d29a2ef8f
Sohan Jambhule <69068073+sohan2503001@users.noreply.github.com>
2023-08-01 09:58:32 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-const blogList = document.getElementById("blogList");
-const postContent = document.getElementById("postContent");
+//const blogList = document.getElementById("blogList");
+//const postContent = document.getElementById("postContent");
 
 const blogPosts = [
   {
@@ -14,52 +14,55 @@ const blogPosts = [
   },
 ];
 
-function displayBlogPosts() {
-  blogList.innerHTML = "";
-
-  blogPosts.forEach((post) => {
-    const postLink = document.createElement("a");
-    postLink.href = `/post/${post.id}`;
-    postLink.textContent = post.title;
-    blogList.appendChild(postLink);
-  });
-}
-
-function displayPostContent(postId) {
-  const post = blogPosts.find((post) => post.id === parseInt(postId, 10));
-
-  if (post) {
-    fetch(`/content/${post.filename}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Blog post content not found.');
-        }
-        return response.text();
-      })
-      .then((content) => {
-        postContent.innerHTML = `
-          <div class="post-content">
-            <h2>${post.title}</h2>
-            <p>${content.replace(/\n/g, '<br>')}</p>
-          </div>
-        `;
-      })
-      .catch((error) => {
-        postContent.innerHTML = `<p>Error loading blog post content: ${error.message}</p>`;
-        console.error(error);
-      });
-  } else {
-    postContent.innerHTML = '<p>Blog post not found.</p>';
+function displayBlogList() {
+  const blogList = document.getElementById('blogList');
+  if (blogList) {
+    blogList.innerHTML = '';
+    blogPosts.forEach((post) => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `<a href="/post/${post.id}">${post.title}</a>`;
+      blogList.appendChild(listItem);
+    });
   }
 }
 
-const path = window.location.pathname;
-const postId = path.split("/post/")[1];
+function displayPostContent(postId) {
+  const postContent = document.getElementById('postContent');
+  if (postContent) {
+    const post = blogPosts.find((post) => post.id === parseInt(postId, 10));
+    if (post) {
+      fetch(`/content/${post.filename}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Blog post content not found.');
+          }
+          return response.text();
+        })
+        .then((content) => {
+          postContent.innerHTML = `
+            <div class="post-content">
+              <h2>${post.title}</h2>
+              <p>${content.replace(/\n/g, '<br>')}</p>
+            </div>
+          `;
+        })
+        .catch((error) => {
+          postContent.innerHTML = `<p>Error loading blog post content: ${error.message}</p>`;
+          console.error(error);
+        });
+    } else {
+      postContent.innerHTML = '<p>Blog post not found.</p>';
+    }
+  }
+}
 
-if (path === "/") {
-  displayBlogPosts();
-} else if (path.startsWith("/post/") && postId) {
+// Check if we are on the index.html page
+if (window.location.pathname === '/') {
+  displayBlogList();
+}
+
+// Check if we are on the post.html page
+if (window.location.pathname.startsWith('/post/')) {
+  const postId = window.location.pathname.split('/post/')[1];
   displayPostContent(postId);
-} else {
-  postContent.innerHTML = "<p>Page not found.</p>";
 }
